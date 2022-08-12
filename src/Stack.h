07404f34428c6e2f2cd4bc84e21a18022e7bf631
Nikola Lazarov <nikola.n.lazarov@outlook.com>
2022-08-12 06:52:37 +0300

    update stack implementation

--- src/Stack.h
+++ src/Stack.h
@@ -1,12 +1,12 @@
 #pragma once
 #include <memory.h>
-#include "LinkedList.h"
+#include <forward_list>
 
 template <class T>
 class Stack
 {
   private:
-    std::shared_ptr<LinkedList<T>> list;
+    std::shared_ptr<std::forward_list<T>> list;
     unsigned int size;
 
   public:
@@ -20,14 +20,13 @@ class Stack
 
 template <class T>
 Stack<T>::Stack() {
-  std::shared_ptr<LinkedList<T>> newList(new LinkedList<T>());
-  list = newList;
+  list = std::shared_ptr<std::forward_list<T>>(new std::forward_list<T>());
   size = 0;
 }
 
 template <class T>
 void Stack<T>::push(T inputData) {
-  list->push(inputData);
+  list->push_front(inputData);
   size += 1;
 }
 
@@ -35,7 +34,7 @@ template <class T>
 void Stack<T>::pop() {
   if (isEmpty()) return;
 
-  list->pop();
+  list->pop_front();
   size -= 1;
 }
 
@@ -43,7 +42,7 @@ template <class T>
 T Stack<T>::top() {
   if (isEmpty()) return NULL;
 
-  return list->getHead()->getData();
+  return list->front();
 }
 
 template <class T>
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,38 @@
 #include <iostream>
 #include "memory.h"
 #include "Stack.h"
+#include <stack>
 
 int main() {
+  /*
+   * Standard Library Stack 
+   */
+  std::cout << "Standard Library Stack" << std::endl << "======================================" << std::endl;
+
+  std::stack<int> stdStackInstance;
+
+  std::cout << "Pushing 1, 2 and 3 to the stack.." << std::endl;
+
+  stdStackInstance.push(1);
+  stdStackInstance.push(2);
+  stdStackInstance.push(3);
+
+  std::cout << "Top of the stack: " << stdStackInstance.top() << std::endl;
+
+  std::cout << "Popping the top element of the stack.." << std::endl;
+
+  stdStackInstance.pop();
+
+  std::cout << "Top of the stack: " << stdStackInstance.top() << std::endl;
+
+  std::cout << "Stack instance size: " << stdStackInstance.size() << std::endl;
+
+
+  /*
+   * Custom Stack Class  
+   */
+  std::cout << std::endl << "Custom Stack Class" << std::endl << "======================================" << std::endl;
+
   Stack<int> stackInstance;
   
   std::cout << "Pushing 1, 2 and 3 to the stack.." << std::endl;  
